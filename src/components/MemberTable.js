04d1280b530f754d5c1c9e216a39a47766698e3c
wendulem <34697277+wendulem@users.noreply.github.com>
2020-04-06 20:46:10 -0400

    Editing and adding functionality

--- src/components/MemberTable.js
+++ src/components/MemberTable.js
@@ -1,34 +1,125 @@
 import React from "react";
-import { Button, Icon, Table, Header, Pagination} from "semantic-ui-react";
+import { Button, Icon, Table, Header, Pagination, Input } from "semantic-ui-react";
+import axios from 'axios';
 import UserInfoModal from "./UserInfoModal";
 
 // Why did I make this a class?
 class MemberTable extends React.Component {
+  state = {
+    activePage: 1,
+    memberTotal: this.props.memberList.length,
+    adding: false,
+    name: null, 
+    netID: null,
+    school: null,
+    major: null
+  };
 
-    state = {
-      activePage: 1,
-      memberTotal: this.props.memberList.length
+  toggleAdding = () => {
+    this.setState({ adding: !this.state.adding }, () => console.log(this.state.adding));
+  }
+
+  addUser = async () => {
+    var mem = {
+      firstName: this.state.name.substring(0, this.state.name.indexOf(' ')), 
+      lastName: this.state.name.substring(this.state.name.indexOf(' ') + 1, this.state.name.length), 
+      netID: this.state.netID,
+      password: '',
+      team: '',
+      major: this.state.major,
+      biography: '',
+      graduationYear: '',
+      school: this.state.school,
+      githubLink: '',
+      linkedIn: '',
+      photoString: ''
+    };
+    
+    let response = await axios.post('https://dukeappml.herokuapp.com//user/new', mem);
+    console.log(response);
+
+    window.location.reload();
+  }
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+  addSaveButton = () => {
+    if (this.state.adding) {
+      return (
+      <Button
+        floated="left"
+        icon
+        labelPosition="left"
+        size="small"
+        color="green"
+        onClick={this.addUser}
+      >
+        <Icon name="save" /> 
+        Save Changes
+        
+      </Button>
+      )
     }
+    return (
+      <Button
+        floated="left"
+        icon
+        labelPosition="left"
+        primary
+        size="small"
+        onClick={this.toggleAdding}
+      >
 
-    handlePaginationChange = (e, { activePage }) => this.setState({ activePage })
+        <Icon name="add user" /> 
+        Add User
 
-    // Have a table class, columns as prop
-    render() {
+      </Button>
+    )
+  }
 
-      const rowCreation = () => this.props.memberList.map(member => {
+  addingUserBlock = () => {
+
+    if (this.state.adding) {
+      return (
+        <Table.Row key='adding'>
+            <Table.Cell>
+              <Input name='name' onChange={this.handleChange} focus placeholder='Name' />
+            </Table.Cell>
+
+            <Table.Cell>
+              <Input name='netID' onChange={this.handleChange} focus placeholder='NetID' />
+            </Table.Cell>
+
+            <Table.Cell>
+              <Input name='school' onChange={this.handleChange} focus placeholder='School' />
+            </Table.Cell>
+
+            <Table.Cell>
+              <Input name='major' onChange={this.handleChange} focus placeholder='Discipline' />
+            </Table.Cell>
+
+        </Table.Row>
+      )
+    }
+  };
+
+  handlePaginationChange = (e, { activePage }) => this.setState({ activePage });
+
+  rowCreation = () =>
+      this.props.memberList.map((member) => {
         // is there a way to map the object to the cells faster?
         // iterate through object
         // HOW TO PUT THIS AS THE LAST ROW EVEN WHEN NULL
         // <Table.Cell><Button active>{member.headshotLink.substring(member.headshotLink.lastIndexOf("/") + 1, member.headshotLink.length)}</Button></Table.Cell>
         // <Button active>N/A</Button>
-  
+
         /*
         <Table.Cell>
             <a href={member.githubLink} rel='noreferrer noopener' target="_blank"><Button icon><Icon name="github" size="large" /></Button></a>
             <a href={member.linkedIn} rel='noreferrer noopener' target="_blank"><Button icon><Icon name="linkedin" size="large" /></Button></a>
           </Table.Cell>
         */
-  
+
         /*
           <Button.Group floated="right">
                   <Button primary>Save</Button>
@@ -36,76 +127,79 @@ class MemberTable extends React.Component {
                   <Button>Cancel</Button>
                 </Button.Group>
         */
-        
-        console.log("test")
-        
-        const newTableRow = (<Table.Row key={member.uid}>
-          <Table.Cell>{member.firstName ? member.firstName : "WARNING"} {member.lastName}</Table.Cell>
-          <Table.Cell>{member.netID ? member.netID : "Please fill, no information"}</Table.Cell>
-          
-          <Table.Cell>{member.school ? member.school : 
-          <Header as='h5' color='red'>
-            <Icon name='arrow circle up' />
-            <Header.Content>Please Update: No information</Header.Content>
-          </Header>}
-          </Table.Cell>
-          
-          <Table.Cell>{member.major}</Table.Cell>
-        </Table.Row>)
-  
-        return (
-          <UserInfoModal trigger={newTableRow} userInfo={member} />
-        );
+
+        return <UserInfoModal trigger={
+          <Table.Row key={member.uid}>
+            <Table.Cell>
+              {member.firstName ? member.firstName : "WARNING"}{" "}
+              {member.lastName}
+            </Table.Cell>
+            <Table.Cell>
+              {member.netID ? member.netID : "Please fill, no information"}
+            </Table.Cell>
+
+            <Table.Cell>
+              {member.school ? (
+                member.school
+              ) : (
+                <Header as="h5" color="red">
+                  <Icon name="arrow circle up" />
+                  <Header.Content>Please Update: No information</Header.Content>
+                </Header>
+              )}
+            </Table.Cell>
+
+            <Table.Cell>{member.major}</Table.Cell>
+          </Table.Row>
+        } 
+        userInfo={member} />;
       });
-    
-      //conditional
 
-      return (
-        <Table celled selectable>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Name</Table.HeaderCell>
-              <Table.HeaderCell>NetID</Table.HeaderCell>
-              <Table.HeaderCell>School</Table.HeaderCell>
-              <Table.HeaderCell>Discipline</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
+  // Have a table class, columns as prop
+  render() {
 
-          <Table.Body>
-            {rowCreation().slice(5 * (this.state.activePage - 1), 5 * (this.state.activePage))}
-          </Table.Body>
+    //conditional
 
-          <Table.Footer fullWidth>
-            <Table.Row>
-              <Table.HeaderCell colSpan="9">
-                <Button
-                  floated="left"
-                  icon
-                  labelPosition="left"
-                  primary
-                  size="small"
-                >
-                  <Icon name="add user" /> Add User
-                </Button>
-              
-                <Pagination
-                  boundaryRange={0}
-                  defaultActivePage={1}
-                  ellipsisItem={null}
-                  firstItem={null}
-                  lastItem={null}
-                  siblingRange={1}
-                  totalPages={Math.ceil(this.props.memberList.length / 5)}
-                  floated="right"
-                  onPageChange={this.handlePaginationChange}
-                />
+    return (
+      <Table celled fixed selectable>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell width={5}>Name</Table.HeaderCell>
+            <Table.HeaderCell width={5}>NetID</Table.HeaderCell>
+            <Table.HeaderCell width={5}>School</Table.HeaderCell>
+            <Table.HeaderCell width={5}>Discipline</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
 
-              </Table.HeaderCell>
-            </Table.Row>
-          </Table.Footer>
-        </Table>
-      );
-    }
+        <Table.Body>
+          {this.rowCreation().slice(
+            5 * (this.state.activePage - 1),
+            5 * this.state.activePage
+          )}
+          {this.addingUserBlock()}
+        </Table.Body>
+
+        <Table.Footer fullWidth>
+          <Table.Row>
+            <Table.HeaderCell colSpan="9">
+              {this.addSaveButton()}
+              <Pagination
+                boundaryRange={0}
+                defaultActivePage={1}
+                ellipsisItem={null}
+                firstItem={null}
+                lastItem={null}
+                siblingRange={1}
+                totalPages={Math.ceil(this.props.memberList.length / 5)}
+                floated="right"
+                onPageChange={this.handlePaginationChange}
+              />
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>
+    );
+  }
 }
 
 export default MemberTable;
--- src/components/UserInfoModal.js
+++ src/components/UserInfoModal.js
@@ -1,12 +1,58 @@
 import React from 'react'
 import { Button, Header, Icon, Modal, Image, Form, Popup, TextArea } from 'semantic-ui-react'
+import axios from 'axios'
 
 class UserInfoModal extends React.Component {
 
-  state = {
-    currentStatus: null
+  state = { 
+    firstName: this.props.userInfo.firstName, 
+    lastName: this.props.userInfo.lastName,
+    netID: this.props.userInfo.netID,
+    password: "",
+    team: "",
+    major: this.props.userInfo.major,
+    biography: this.props.userInfo.biography,
+    graduationYear: "",
+    school: this.props.userInfo.school,
+    githubLink: this.props.userInfo.githubLink,
+    linkedIn: this.props.userInfo.linkedIn,
+    photoString: ""
+    //submittedFirstName: null, 
+    //submittedLastName: null 
   }
 
+  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+  handleSubmit = () => {
+    //const { firstName, lastName } = this.state
+    //this.setState({ submittedFirstName: firstName, submittedLastName: lastName }, () => console.log(this.state.submittedFirstName))
+    this.editMember()
+  }
+
+  editMember =  async () => {
+    //this.setState({buttontext: "Loading..."});
+    //let photoLink = await this.imageUpload(event);
+
+    var mem = this.state
+    console.log(mem);
+    let URL = 'https://dukeappml.herokuapp.com//user/' + this.props.userInfo.uid;
+    console.log(URL)
+    let response = await axios.put(URL, mem)
+    console.log(response)
+
+    window.location.reload(); // might just make these exit the modal and reload elsewhere? who knows
+  }
+
+  deleteMember = async () => {
+    let URL = 'https://dukeappml.herokuapp.com//user/' + this.props.userInfo.uid;
+    let response = await axios.delete(URL);
+    
+    console.log(response)
+
+    window.location.reload();
+  }
+
+  
   render() {
     return(
       <Modal trigger={this.props.trigger} closeIcon>
@@ -15,61 +61,65 @@ class UserInfoModal extends React.Component {
         <Modal.Content>
 
           <Image wrapped size='medium' centered src={this.props.userInfo.photoString} />
+          
           <br/>
-            <Modal.Description>
-              <Header textAlign='center'>{this.props.userInfo.firstName} {this.props.userInfo.lastName}</Header>
-            </Modal.Description>
+          
+          <Modal.Description>
+            <Header textAlign='center'>{this.props.userInfo.firstName} {this.props.userInfo.lastName}</Header>
+          </Modal.Description>
+          
+          <br />
 
-        <Form>
-            <Form.Field>
-              <label>First Name</label>
-              <input placeholder='First name' defaultValue={this.props.userInfo.firstName} />
-            </Form.Field>
+          <Button icon style={{display: "block", "margin-left": "auto", "margin-right": "auto"}} labelPosition='left'>
+            Upload Photo
+            <Icon name='cloud upload' />
+          </Button>
 
-            <Form.Field>
-              <label>Last Name</label>
-              <input placeholder='Last Name' defaultValue={this.props.userInfo.lastName} />
-            </Form.Field>
+          <Form>
+              <Form.Field>
+                <label>First Name</label>
+                <Form.Input name='firstName' onChange={this.handleChange} placeholder='First name' defaultValue={this.props.userInfo.firstName} />
+              </Form.Field>
 
-            <Form.Field>
-              <label>Last Name</label>
-              <input placeholder='Last Name' defaultValue={this.props.userInfo.lastName} />
-            </Form.Field>
+              <Form.Field>
+                <label>Last Name</label>
+                <Form.Input name='lastName' onChange={this.handleChange} placeholder='Last Name' defaultValue={this.props.userInfo.lastName} />
+              </Form.Field>
 
-            <Form.Field>
-              <label>NetID</label>
-              <input placeholder='NetID' defaultValue={this.props.userInfo.netID} />
-            </Form.Field>
+              <Form.Field>
+                <label>NetID</label>
+                <Form.Input name='netID' onChange={this.handleChange} placeholder='NetID' defaultValue={this.props.userInfo.netID} />
+              </Form.Field>
 
-            <Form.Field>
-              <label>School</label>
-              <input placeholder='School' defaultValue={this.props.userInfo.school} />
-            </Form.Field>
+              <Form.Field>
+                <label>School</label>
+                <Form.Input name='school' onChange={this.handleChange} placeholder='School' defaultValue={this.props.userInfo.school} />
+              </Form.Field>
 
-            <Form.Field>
-              <label>Discipline</label>
-              <input placeholder='Discipline' defaultValue={this.props.userInfo.major} />
-            </Form.Field>
+              <Form.Field>
+                <label>Discipline</label>
+                <Form.Input name='major' onChange={this.handleChange} placeholder='Discipline' defaultValue={this.props.userInfo.major} />
+              </Form.Field>
 
-            <Form.Field>
-              <label>LinkedIn <a href={this.props.userInfo.linkedIn} rel='noreferrer noopener' target="_blank"><Icon name="external alternate" size="small" /></a> </label>
-              <input placeholder='LinkedIn' defaultValue={this.props.userInfo.linkedIn} />
-            </Form.Field>
+              <Form.Field>
+                <label>LinkedIn <a href={this.state.linkedIn} rel='noreferrer noopener' target="_blank"><Icon name="external alternate" size="small" /></a> </label>
+                <Form.Input name='linkedIn' onChange={this.handleChange} placeholder='LinkedIn' defaultValue={this.props.userInfo.linkedIn} />
+              </Form.Field>
 
-            <Form.Field>
-              <label>GitHub <a href={this.props.userInfo.githubLink} rel='noreferrer noopener' target="_blank"><Icon name="external alternate" size="small" /></a> </label>
-              <input placeholder='GitHub' defaultValue={this.props.userInfo.githubLink} />
-            </Form.Field>
+              <Form.Field>
+                <label>GitHub <a href={this.state.githubLink} rel='noreferrer noopener' target="_blank"><Icon name="external alternate" size="small" /></a> </label>
+                <Form.Input name='githubLink' onChange={this.handleChange} placeholder='GitHub' defaultValue={this.props.userInfo.githubLink} />
+              </Form.Field>
 
-            <TextArea placeholder='Tell us about yourself...' defaultValue={this.props.userInfo.biography} />
+              <TextArea name='biography' onChange={this.handleChange} placeholder='Tell us about yourself...' defaultValue={this.props.userInfo.biography} />
 
-          </Form>
+            </Form>
 
         </Modal.Content>
 
         <Modal.Actions>   
           
-          <Button color='green'>
+          <Button onClick={this.handleSubmit} color='green'>
             <Icon name='save' /> Save Changes       
           </Button>
 
@@ -79,7 +129,7 @@ class UserInfoModal extends React.Component {
                 <Icon name='delete' /> Delete User
               </Button>
             }
-            content={<Button color='red' content='Confirm user deletion?' />}
+            content={<Button color='red' content='Confirm user deletion?' onClick={this.deleteMember} />}
             on='click'
             position='top right'
           />
