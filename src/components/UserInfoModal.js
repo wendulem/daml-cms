784d8bff2b8e5abde137c383cfb35b3c1df94a6c
wendulem <34697277+wendulem@users.noreply.github.com>
2020-04-13 14:39:50 -0400

    Upload API work

--- src/components/UserInfoModal.js
+++ src/components/UserInfoModal.js
@@ -1,11 +1,25 @@
-import React from 'react'
-import { Button, Header, Icon, Modal, Image, Form, Popup, TextArea, Input } from 'semantic-ui-react'
-import axios from 'axios'
+import React from "react";
+import {
+  Button,
+  Header,
+  Icon,
+  Modal,
+  Image,
+  Form,
+  Popup,
+  TextArea,
+  Grid,
+  Loader,
+} from "semantic-ui-react";
+import axios from "axios";
 
 class UserInfoModal extends React.Component {
+  state = {
+    uploadStatus: null,
+    file: "",
+    fileName: "",
 
-  state = { 
-    firstName: this.props.userInfo.firstName, 
+    firstName: this.props.userInfo.firstName,
     lastName: this.props.userInfo.lastName,
     netID: this.props.userInfo.netID,
     password: "",
@@ -16,155 +30,259 @@ class UserInfoModal extends React.Component {
     school: this.props.userInfo.school,
     githubLink: this.props.userInfo.githubLink,
     linkedIn: this.props.userInfo.linkedIn,
-    photoString: ""
-    //submittedFirstName: null, 
-    //submittedLastName: null 
-  }
+    photoString: this.props.userInfo.photoString,
+  };
 
-  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+  checkButtonText = () => {
+    if (this.state.uploadStatus === "uploaded") {
+      return this.fileName;
+    }
+    if (this.state.uploadStatus === "loading") {
+      return <Loader content="Loading" />;
+    }
+    return "Upload Photo";
+  };
 
-  handleSubmit = () => {
+  imageUpload = async () => {
+
+    console.log("imageUpload");
+    
+    this.setState({ uploadStatus: "loading" });
+
+    var fd = new FormData();
+    fd.append('file', this.state.file);
+    fd.append('upload_preset', 'owcbovwd');
+    
+    //Why doesn't any of this work...
+
+    /*const res = await fetch(
+      ' https://api.cloudinary.com/v1_1/dfitae3co/image/upload',
+      {
+        method: 'POST',
+        body: fd
+      }
+    )
+
+    const file = await res.json()
+
+    console.log(file.secure_url)*/
+    
+
+    axios({
+      url:'https://api.cloudinary.com/v1_1/dfitae3co/upload',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: fd
+    }).then( (res) => {
+      console.log(res)
+    }).catch( (err) => {
+      console.error(err)
+    })
+  };
+
+  handleInfoChange = (e, { name, value }) => this.setState({ [name]: value });
+
+  handleFileSubmit = () => {
     //const { firstName, lastName } = this.state
     //this.setState({ submittedFirstName: firstName, submittedLastName: lastName }, () => console.log(this.state.submittedFirstName))
-    this.editMember()
-  }
+    this.editMember();
+  };
 
-  editMember =  async () => {
+  fileChange = (e) => {
+    console.log("fileChange");
+    this.setState(
+      { file: e.target.files[0], fileName: e.target.files[0].name },
+      () => {
+        console.log(
+          "File chosen --->",
+          this.state.file,
+          console.log("File name  --->", this.state.fileName)
+        );
+      }
+    );
+
+    this.imageUpload();
+  };
+
+  editMember = async () => {
     //this.setState({buttontext: "Loading..."});
     //let photoLink = await this.imageUpload(event);
 
-    var mem = this.state
+    var mem = this.state;
     console.log(mem);
-    let URL = 'https://dukeappml.herokuapp.com//user/' + this.props.userInfo.uid;
-    console.log(URL)
-    let response = await axios.put(URL, mem)
-    console.log(response)
+    let URL =
+      "https://dukeappml.herokuapp.com//user/" + this.props.userInfo.uid;
+    console.log(URL);
+    let response = await axios.put(URL, mem);
+    console.log(response);
 
     window.location.reload(); // might just make these exit the modal and reload elsewhere? who knows
-  }
+  };
 
   deleteMember = async () => {
-    let URL = 'https://dukeappml.herokuapp.com//user/' + this.props.userInfo.uid;
+    let URL =
+      "https://dukeappml.herokuapp.com//user/" + this.props.userInfo.uid;
     let response = await axios.delete(URL);
-    
-    console.log(response)
+
+    console.log(response);
 
     window.location.reload();
-  }
-
-  // Fix this for axios, etc.
-  tempImageUpload = async (e) => {
-    const files = e.target.files
-    const data = new FormData()
-    data.append('file', files[0])
-    data.append('upload_preset', 'damlImages')
-
-    const res = await fetch(
-      'https://api.cloudinary.com/v1_1/dndwfzjzr/image/upload', 
-      {
-        method: 'POST',
-        body: data
-      }
-    ) 
-
-    const file = await res.json()
-    console.log(file);
-    this.setState({photoString: "https://res.cloudinary.com/dndwfzjzr/" + file.public_id});
-  }
-
-  
-  /*
-       <Input onChange = {this.handleFile} type="file">
-            Upload Photo
-            <Icon name='cloud upload' />
-          </Input>
-  */
+  };
 
   render() {
-    return(
+    return (
       <Modal trigger={this.props.trigger} closeIcon>
-        <Header icon='user circle' content='Detailed User View' />
+        <Header icon="user circle" content="Detailed User View" />
 
         <Modal.Content>
+          <Image wrapped size="medium" centered src={this.state.photoString} />
 
-          <Image wrapped size='medium' centered src={this.props.userInfo.photoString} />
-          
-          <br/>
-          
-          <Modal.Description>
-            <Header textAlign='center'>{this.props.userInfo.firstName} {this.props.userInfo.lastName}</Header>
-          </Modal.Description>
-          
           <br />
 
-          <Button icon onClick={this.tempImageUpload} style={{display: "block", "margin-left": "auto", "margin-right": "auto"}} labelPosition='left'>
-            Upload Photo
-            <Icon name='cloud upload' />
-          </Button>
+          <Modal.Description>
+            <Header textAlign="center">
+              {this.props.userInfo.firstName} {this.props.userInfo.lastName}
+            </Header>
+          </Modal.Description>
+
+          <br />
+
+          <Grid>
+            <Grid.Column textAlign="center">
+              <Button icon as="label" htmlFor="file" labelPosition="left">
+                {this.checkButtonText()}
+                <Icon name="cloud upload" />
+              </Button>
+            </Grid.Column>
+          </Grid>
+
+          <input type="file" id="file" hidden onChange={this.fileChange} />
 
           <Form>
-              <Form.Field>
-                <label>First Name</label>
-                <Form.Input name='firstName' onChange={this.handleChange} placeholder='First name' defaultValue={this.props.userInfo.firstName} />
-              </Form.Field>
+            <Form.Field>
+              <label>First Name</label>
+              <Form.Input
+                name="firstName"
+                onChange={this.handleInfoChange}
+                placeholder="First name"
+                defaultValue={this.props.userInfo.firstName}
+              />
+            </Form.Field>
 
-              <Form.Field>
-                <label>Last Name</label>
-                <Form.Input name='lastName' onChange={this.handleChange} placeholder='Last Name' defaultValue={this.props.userInfo.lastName} />
-              </Form.Field>
+            <Form.Field>
+              <label>Last Name</label>
+              <Form.Input
+                name="lastName"
+                onChange={this.handleInfoChange}
+                placeholder="Last Name"
+                defaultValue={this.props.userInfo.lastName}
+              />
+            </Form.Field>
 
-              <Form.Field>
-                <label>NetID</label>
-                <Form.Input name='netID' onChange={this.handleChange} placeholder='NetID' defaultValue={this.props.userInfo.netID} />
-              </Form.Field>
+            <Form.Field>
+              <label>NetID</label>
+              <Form.Input
+                name="netID"
+                onChange={this.handleInfoChange}
+                placeholder="NetID"
+                defaultValue={this.props.userInfo.netID}
+              />
+            </Form.Field>
 
-              <Form.Field>
-                <label>School</label>
-                <Form.Input name='school' onChange={this.handleChange} placeholder='School' defaultValue={this.props.userInfo.school} />
-              </Form.Field>
+            <Form.Field>
+              <label>School</label>
+              <Form.Input
+                name="school"
+                onChange={this.handleInfoChange}
+                placeholder="School"
+                defaultValue={this.props.userInfo.school}
+              />
+            </Form.Field>
 
-              <Form.Field>
-                <label>Discipline</label>
-                <Form.Input name='major' onChange={this.handleChange} placeholder='Discipline' defaultValue={this.props.userInfo.major} />
-              </Form.Field>
+            <Form.Field>
+              <label>Discipline</label>
+              <Form.Input
+                name="major"
+                onChange={this.handleInfoChange}
+                placeholder="Discipline"
+                defaultValue={this.props.userInfo.major}
+              />
+            </Form.Field>
 
-              <Form.Field>
-                <label>LinkedIn <a href={this.state.linkedIn} rel='noreferrer noopener' target="_blank"><Icon name="external alternate" size="small" /></a> </label>
-                <Form.Input name='linkedIn' onChange={this.handleChange} placeholder='LinkedIn' defaultValue={this.props.userInfo.linkedIn} />
-              </Form.Field>
+            <Form.Field>
+              <label>
+                LinkedIn{" "}
+                <a
+                  href={this.state.linkedIn}
+                  rel="noreferrer noopener"
+                  target="_blank"
+                >
+                  <Icon name="external alternate" size="small" />
+                </a>{" "}
+              </label>
+              <Form.Input
+                name="linkedIn"
+                onChange={this.handleInfoChange}
+                placeholder="LinkedIn"
+                defaultValue={this.props.userInfo.linkedIn}
+              />
+            </Form.Field>
 
-              <Form.Field>
-                <label>GitHub <a href={this.state.githubLink} rel='noreferrer noopener' target="_blank"><Icon name="external alternate" size="small" /></a> </label>
-                <Form.Input name='githubLink' onChange={this.handleChange} placeholder='GitHub' defaultValue={this.props.userInfo.githubLink} />
-              </Form.Field>
+            <Form.Field>
+              <label>
+                GitHub{" "}
+                <a
+                  href={this.state.githubLink}
+                  rel="noreferrer noopener"
+                  target="_blank"
+                >
+                  <Icon name="external alternate" size="small" />
+                </a>{" "}
+              </label>
+              <Form.Input
+                name="githubLink"
+                onChange={this.handleInfoChange}
+                placeholder="GitHub"
+                defaultValue={this.props.userInfo.githubLink}
+              />
+            </Form.Field>
 
-              <TextArea name='biography' onChange={this.handleChange} placeholder='Tell us about yourself...' defaultValue={this.props.userInfo.biography} />
-
-            </Form>
-
+            <TextArea
+              name="biography"
+              onChange={this.handleInfoChange}
+              placeholder="Tell us about yourself..."
+              defaultValue={this.props.userInfo.biography}
+            />
+          </Form>
         </Modal.Content>
 
-        <Modal.Actions>   
-          
-          <Button onClick={this.handleSubmit} color='green'>
-            <Icon name='save' /> Save Changes       
+        <Modal.Actions>
+          <Button onClick={this.handleFileSubmit} color="green">
+            <Icon name="save" /> Save Changes
           </Button>
 
           <Popup
             trigger={
-              <Button color='red'>
-                <Icon name='delete' /> Delete User
+              <Button color="red">
+                <Icon name="delete" /> Delete User
               </Button>
             }
-            content={<Button color='red' content='Confirm user deletion?' onClick={this.deleteMember} />}
-            on='click'
-            position='top right'
+            content={
+              <Button
+                color="red"
+                content="Confirm user deletion?"
+                onClick={this.deleteMember}
+              />
+            }
+            on="click"
+            position="top right"
           />
         </Modal.Actions>
-
       </Modal>
-    )
+    );
   }
 }
 
-export default UserInfoModal
+export default UserInfoModal;
